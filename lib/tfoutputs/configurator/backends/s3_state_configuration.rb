8c1c4a5ec8411cb74d0101cb32bb7869756c8694
steakfest <john@steakfest.com>
2018-06-01 10:02:09 -0600

    Add aws credential profile to s3 state configuration
    It can be common for people to have to manage multiple aws accounts and being able to specify which aws credential profile when using this gem is necessary for them.

--- lib/tfoutputs/configurator/backends/s3_state_configuration.rb
+++ lib/tfoutputs/configurator/backends/s3_state_configuration.rb
@@ -10,11 +10,12 @@ module TfOutputs
           @bucket_name = options[:bucket_name]
           @bucket_key = options[:bucket_key]
           @bucket_region = options[:bucket_region]
+          @profile = options[:profile]
         end
 
         def save
           file = Tempfile.new('tf_state')
-          s3 = Aws::S3::Client.new(region: @bucket_region)
+          s3 = Aws::S3::Client.new(region: @bucket_region, profile: @profile)
           resp = s3.get_object bucket: @bucket_name, key: @bucket_key
           file.write(resp.body.string)
           file.rewind
